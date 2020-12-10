075c3f799a710876796781b90a747337c449e4d9
Eric F. Lock <Eric.F.Lock@gmail.com>
2020-12-10 14:47:06 -0600

    Update bidifac.plus.R
    Fixed bugs

--- bidifac.plus.R
+++ bidifac.plus.R
@@ -1,14 +1,14 @@
 bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,conv.thresh=0.001,temp.iter=100){
-# X0=X0; p.ind = p.ind; n.ind=n.ind; max.comb=20; num.comp=20; max.iter=500; conv.thresh=1; temp.iter=100
+  # X0=X0; p.ind = p.ind; n.ind=n.ind; max.comb=20; num.comp=20; max.iter=500; conv.thresh=1; temp.iter=100
   n.source <- length(p.ind)
   n.type <- length(n.ind)
   X0.resid <- X0
   S <- list()
   pen <- c()
-    n.ind.list <- list()
-    p.ind.list <- list()
+  n.ind.list <- list()
+  p.ind.list <- list()
   for(i in 1:max.comb){
-    S[[i]]=0
+    S[[i]]=array(rep(0,prod(dim(X0))),dim=dim(X0))
     pen[i]=0
     n.ind.list[[i]]<-c(1:dim(X0)[2])
     p.ind.list[[i]]<-c(1:dim(X0)[1])}
@@ -22,8 +22,8 @@ bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,con
       lambda <- 1+(temp.iter-1-jj)/temp.iter*temp.fac
     }
     
-   for(k in 1:max.comb){
-  #    print(paste(k,'*'))
+    for(k in 1:max.comb){
+      #    print(paste(k,'*'))
       X0.temp <- X0.resid+S[[k]]
       cur.n.ind <- n.ind.list[[k]]#c(1:dim(X0.temp)[2])
       cur.p.ind <- c()
@@ -34,7 +34,7 @@ bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,con
         res.p <- bidifac.cycle(X0.temp=X0.temp,x.ind=p.ind,num.comp=num.comp, lambda=lambda)
         cur.p.ind <- sort(res.p$cur.x.ind)
         if(isTRUE(all.equal(prev.p.ind,cur.p.ind))&&isTRUE(all.equal(prev.n.ind,cur.n.ind))) break
-          X0.temp.n <- t(X0.temp[cur.p.ind,])
+        X0.temp.n <- t(X0.temp[cur.p.ind,])
         res.n<- bidifac.cycle(X0.temp=X0.temp.n,x.ind=n.ind,num.comp=num.comp,lambda=lambda)
         cur.n.ind <- sort(res.n$cur.x.ind)
         if(isTRUE(all.equal(prev.p.ind,cur.p.ind))&&isTRUE(all.equal(prev.n.ind,cur.n.ind))) break
@@ -46,7 +46,7 @@ bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,con
         Old.a <- svd(X0.temp[p.ind.list[[k]],n.ind.list[[k]]],nu=0,nv=0)$d
         New.a <- svd(X0.temp[cur.p.ind,cur.n.ind],nu=0,nv=0)$d
         OldPen <- sum(pmax(Old.a-lambda*(sqrt(length(p.ind.list[[k]]))+sqrt(length(n.ind.list[[k]]))),0)^2)
-        NewPen <- sum(pmax(New.a-lambda*(sqrt(length(p.ind.list[[k]]))+sqrt(length(n.ind.list[[k]]))),0)^2)
+        NewPen <- sum(pmax(New.a-lambda*(sqrt(length(cur.p.ind))+sqrt(length(cur.n.ind))),0)^2)
         if(OldPen>NewPen){ 
           CurPen <- OldPen  
           cur.p.ind = p.ind.list[[k]]
@@ -55,11 +55,11 @@ bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,con
       }
       Repeat=FALSE
       if(k>1){
-      for(j in 1:(k-1)){
-        if(isTRUE(all.equal(p.ind.list[[j]],cur.p.ind))&&isTRUE(all.equal(n.ind.list[[j]],cur.n.ind))){
-          Repeat=TRUE
-        }
-      }}
+        for(j in 1:(k-1)){
+          if(isTRUE(all.equal(p.ind.list[[j]],cur.p.ind))&&isTRUE(all.equal(n.ind.list[[j]],cur.n.ind))){
+            Repeat=TRUE
+          }
+        }}
       if(CurPen<=0.001|Repeat){
         pen = pen[-k]
         S[[k]] <- NULL
@@ -98,6 +98,7 @@ bidifac.plus <- function(X0,p.ind,n.ind,max.comb=20,num.comp=20,max.iter=500,con
   return(list(S=S,p.ind.list=p.ind.list,n.ind.list=n.ind.list,Sums=Sums,obj.vec=obj.vec))
 }
 
+
 bidifac.cycle <- function(X0.temp,x.ind,num.comp,lambda){
   n <- dim(X0.temp)[2]
   n.x <- length(x.ind)
@@ -133,12 +134,12 @@ bidifac.cycle <- function(X0.temp,x.ind,num.comp,lambda){
   }
   Order = order(sse.pen.tot, decreasing=TRUE)
   i=1
- # for(i in 1:length(sse.pen.tot)){
-    cur.x.ind <- cur.x.ind.list[[Order[i]]]
-    CurPen <- sse.pen.tot[Order[i]]
- #   if(!(list(sort(cur.p.ind))%in%p.ind.list)) break
-#    print('GGG!')
-#  }
-    return(list(cur.x.ind=cur.x.ind,CurPen=CurPen))
+  # for(i in 1:length(sse.pen.tot)){
+  cur.x.ind <- cur.x.ind.list[[Order[i]]]
+  CurPen <- sse.pen.tot[Order[i]]
+  #   if(!(list(sort(cur.p.ind))%in%p.ind.list)) break
+  #    print('GGG!')
+  #  }
+  return(list(cur.x.ind=cur.x.ind,CurPen=CurPen))
 }
 
